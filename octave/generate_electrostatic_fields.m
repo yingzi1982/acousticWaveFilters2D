1c337dbe05071a5eed9f9a83fd74eb07f2e70bcb
yingzi1982 <yingzi.ying@me.com>
2022-05-19 14:31:55 +0100

    pushing to Github

--- octave/generate_electrostatic_fields.m
+++ octave/generate_electrostatic_fields.m
@@ -70,7 +70,7 @@ negative_finger_V = 0;
 V = zeros(size(X));
 
 norm_V = 0;
-norm_residual = 0.00001;
+norm_residual = 0.000005;
 iterationNumber=100000;
 for i = 1:iterationNumber
 %fixed potential
