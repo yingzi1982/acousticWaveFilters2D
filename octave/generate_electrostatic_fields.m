25bbc06b6464099f4b721a9bc05abbb3aa6d0c59
yingzi1982 <yingzi.ying@me.com>
2022-05-23 13:43:49 +0100

    pushing to Github

--- octave/generate_electrostatic_fields.m
+++ octave/generate_electrostatic_fields.m
@@ -62,8 +62,8 @@ negative_finger = [negative_finger_x' negative_finger_z'];
 dlmwrite('../backup/positive_finger',positive_finger,' ');
 dlmwrite('../backup/negative_finger',negative_finger,' ');
 positive_finger_V = 1;
-%negative_finger_V = -1;
-negative_finger_V = 0;
+negative_finger_V = -1;
+%negative_finger_V = 0;
 %---------------------------------
 
 [X Z] = ndgrid (x,z);
@@ -80,15 +80,15 @@ for i = 1:iterationNumber
 % update potential
   V(2:end-1,2:end-1) = (V(1:end-2,2:end-1) + V(3:end,2:end-1) + V(2:end-1,1:end-2) + V(2:end-1,3:end))/4; % inner domain
 
-  %V(1,2:end-1) = (V(1,3:end) + V(1,1:end-2) + V(2,2:end-1))/3; % left edge
-  %V(end,2:end-1) = (V(end,3:end) + V(end,1:end-2) + V(end-1,2:end-1))/3; % right edge
-  %V(2:end-1,1) = (V(1:end-2,1) + V(3:end,1) + V(2:end-1,2))/3; % bottom edge
-  %V(2:end-1,end) = (V(1:end-2,end) + V(3:end,end) + V(2:end-1,end-1))/3; % top edge
+  V(1,2:end-1) = (V(1,3:end) + V(1,1:end-2) + V(2,2:end-1))/3; % left edge
+  V(end,2:end-1) = (V(end,3:end) + V(end,1:end-2) + V(end-1,2:end-1))/3; % right edge
+  V(2:end-1,1) = (V(1:end-2,1) + V(3:end,1) + V(2:end-1,2))/3; % bottom edge
+  V(2:end-1,end) = (V(1:end-2,end) + V(3:end,end) + V(2:end-1,end-1))/3; % top edge
 
-  %V(1,1) = (V(1,2) + V(2,1))/2; % left bottom corner
-  %V(1,end) = (V(2,end) + V(1,end-1))/2; % left top corner
-  %V(end,1) = (V(end-1,1) + V(end,2))/2; % right bottom corner
-  %V(end,end) = (V(end-1,end) + V(end,end-1))/2; % right top corner
+  V(1,1) = (V(1,2) + V(2,1))/2; % left bottom corner
+  V(1,end) = (V(2,end) + V(1,end-1))/2; % left top corner
+  V(end,1) = (V(end-1,1) + V(end,2))/2; % right bottom corner
+  V(end,end) = (V(end-1,end) + V(end,end-1))/2; % right top corner
 
   norm_V_new = norm(V);
   if (norm_V_new - norm_V)/norm_V_new < norm_residual
