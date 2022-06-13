ed21104426fce73d4f96c9d9d381cd9d0503ebf8
Yingzi1982 <yingzi.ying@me.com>
2022-06-13 21:51:11 +0200

    pushing to Github

--- gmt/test.sh
+++ gmt/test.sh
@@ -1,24 +1,41 @@
 #!/usr/bin/env bash
-
 source /pdc/software/21.11/eb/software/Anaconda3/2021.05/bin/activate
 conda activate gmt6
-#		GMT EXAMPLE 13
+
+#               GMT EXAMPLE 38
 #
-# Purpose:	Illustrate vectors and contouring
-# GMT modules:	set, grdmath, grdcontour, grdvector, subplot
-# Unix progs:	rm
+# Purpose:      Illustrate histogram equalization on topography grids
+# GMT modules:  colorbar, text, makecpt, grdhisteq, grdimage
+# Unix progs:   rm
 #
-gmt begin ex13
-	gmt set FONT_HEADING 40p,Times-Italic
-	gmt grdmath -R-2/2/-2/2 -I0.1 X Y R2 NEG EXP X MUL = z.nc
-	gmt grdmath z.nc DDX = dzdx.nc
-	gmt grdmath z.nc DDY = dzdy.nc
-	gmt subplot begin 2x2 -M0.1c -Ff15c -BWSne -T"z(x,y) = x@~\327@~exp(-x@+2@+-y@+2@+)"
-		gmt grdcontour z.nc -C0.05 -A0.1 -Gd5c -S4 -T+d8p/2p -c0,0
-		gmt grdcontour z.nc -C0.05 -Gd5c -S4 -c0,1
-		gmt grdvector dzdx.nc dzdy.nc -I0.2 -Q0.25c+e+n0.25i/0+h0.5 -Gblack -W1p -S2c
-		gmt grdcontour dzdx.nc -C0.10 -A0.5 -Gd5c -S4 -T+d8p/2p -c1,0
-		gmt grdcontour dzdy.nc -C0.05 -A0.2 -Gd5c -S4 -T+d8p/2p -c1,1
+gmt begin ex38
+	gmt set FONT_TAG 14p
+
+	gmt makecpt -Crainbow -T0/1700 -H > t.cpt
+	gmt makecpt -Crainbow -T0/15/1 -H > c.cpt
+	gmt makecpt -Crainbow -T-3/3 -H > n.cpt
+	gmt makecpt -Crainbow -T0/15 -H > q.cpt
+	gmt grdcut @earth_relief_02m -R-60/-45/-20/-10 -Gtopo_38.nc
+
+	gmt subplot begin 2x2 -Fs7c -A+jTR+gwhite+p1p+o0.2c/0.2c -M0.75c/1.2c -Rtopo_38.nc -JM7c -B5 -BWSen -Y10c
+		gmt subplot set 0 -A"Original"
+		gmt grdimage topo_38.nc -I+d -Ct.cpt
+
+		gmt subplot set 1 -A"Equalized"
+		gmt grdhisteq topo_38.nc -Gout.nc -C16
+		gmt grdimage out.nc -Cc.cpt
+
+		gmt subplot set 2 -A"Normalized"
+		gmt grdhisteq topo_38.nc -Gout.nc -N
+		gmt grdimage out.nc -Cn.cpt
+
+		gmt subplot set 3 -A"Quadratic"
+		gmt grdhisteq topo_38.nc -Gout.nc -Q
+		gmt grdimage out.nc -Cq.cpt
 	gmt subplot end
+
+	gmt colorbar -DJTC+w12c/0.4c+jTC+o0c/6c+h+e+n -Ct.cpt -Bxa500 -By+lm
+	gmt colorbar -DJBC+w12c/0.4c+h+e+n -Cn.cpt -Bx1 -By+l"z@-n@-"
+	gmt colorbar -DJBC+w12c/0.4c+h+e+n+o0c/2.5c -Cq.cpt -Bx1 -By+l"z@-q@-"
+	rm -f out.nc topo_38.nc ?.cpt
 gmt end 
-rm -f z.nc dzdx.nc dzdy.nc
