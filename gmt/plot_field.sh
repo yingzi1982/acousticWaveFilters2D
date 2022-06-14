cbc2cdc21057a92a86ba2c3e9602bc8a77bfe44e
Yingzi1982 <yingzi.ying@me.com>
2022-06-14 10:09:07 +0200

    pushing to Github

--- gmt/plot_field.sh
+++ gmt/plot_field.sh
@@ -60,7 +60,8 @@ gmt begin $fig pdf
 gmt makecpt -C$cpt -T$lowerLimit/$upperLimit -Iz
 
 gmt basemap -R$region -J$projection -BWeSn -Bx10f5+l"X ($unit\m) " -By10f5+l"Z ($unit\m)"
-awk -v unit="$unit" -v scale="$scale" '{print $1/unit, $2/unit, $3/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$lowerLimit -Lu$upperLimit -R$region -I$inc -G$grd
+#awk -v unit="$unit" -v scale="$scale" '{print $1/unit, $2/unit, $3/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$lowerLimit -Lu$upperLimit -R$region -I$inc -G$grd
+awk -v unit="$unit" -v scale="$scale" '{print $1/unit, $2/unit, $3/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface  -R$region -I$inc -G$grd
 gmt grdimage $grd
 
 if [ $column_number -eq 6 ]
