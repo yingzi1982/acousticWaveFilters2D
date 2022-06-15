d49c8aa2ed4ebe87d3a31b7cde2540520afabd3b
Yingzi1982 <yingzi.ying@me.com>
2022-06-15 14:12:47 +0200

    pushing to Github

--- bash/gmt.sh
+++ bash/gmt.sh
@@ -2,6 +2,6 @@
 
 cd ../gmt
 
-#./plot_fields.sh potential 1E-6 1E0 V 
-#./plot_fields.sh electric 1E-6 1E6 V/m
+./plot_fields.sh potential 1E-6 1E0 V 
+./plot_fields.sh electric 1E-6 1E6 V/m
 ./plot_fields.sh bodyforce 1E-6 1E13 N/m@+2@+
