b869a275b91336eac334f79d2613c586e20c36e8
Yingzi1982 <yingzi.ying@me.com>
2022-06-16 10:51:06 +0200

    pushing to Github

--- gmt/plot_fields.sh
+++ gmt/plot_fields.sh
@@ -2,35 +2,7 @@
 #module load gmt
 source /pdc/software/21.11/eb/software/Anaconda3/2021.05/bin/activate
 conda activate gmt6
-gmt defaults
-exit
-
-rm -f gmt.conf
-rm -f gmt.history
-
-gmt gmtset MAP_FRAME_AXES WeSn
-#gmt gmtset MAP_FRAME_TYPE plain
-#gmt gmtset MAP_FRAME_PEN thick
-#gmt gmtset MAP_TICK_PEN_PRIMARY thin
-#gmt gmtset MAP_TICK_PEN_SECONDARY thinner
-#gmt gmtset MAP_TICK_LENGTH_PRIMARY -3p
-#gmt gmtset MAP_DEGREE_SYMBOL none
-#gmt gmtset MAP_GRID_CROSS_SIZE_PRIMARY 0.0i
-#gmt gmtset MAP_GRID_CROSS_SIZE_SECONDARY 0.0i
-#gmt gmtset MAP_GRID_PEN_PRIMARY thin,black
-#gmt gmtset MAP_GRID_PEN_SECONDARY thin,black
-gmt gmtset MAP_ORIGIN_X 100p
-gmt gmtset MAP_ORIGIN_Y 100p
-#gmt gmtset FORMAT_GEO_OUT +D
-gmt gmtset COLOR_NAN 255/255/255
-gmt gmtset COLOR_FOREGROUND 255/255/255
-gmt gmtset COLOR_BACKGROUND 0/0/0
-gmt gmtset FONT 12p,Helvetica,black
-#gmt gmtset FONT 9p,Times-Roman,black
-#gmt gmtset PS_MEDIA custom_2.8ix2.8i
-gmt gmtset PS_MEDIA letter
-gmt gmtset PS_PAGE_ORIENTATION portrait
-#gmt gmtset GMT_VERBOSE d
+#gmt defaults
 
 #--------------------------------------------------------------------
 name=$1
@@ -43,20 +15,17 @@ backupFolder=../backup/
 DATAFolder=../DATA/
 figFolder=../figures/
 mkdir -p $figFolder
-
-ps=$figFolder$name.ps
-pdf=$figFolder$name.pdf
-cpt=$backupFolder$name\.cpt
+fig=$figFolder$name
 
 originalxyz=$backupFolder$name
 grd=$backupFolder$name\.nc
 xgrd=$backupFolder$name\_x.nc
 zgrd=$backupFolder$name\_z.nc
 
-xmin=`gmt gmtinfo $originalxyz -C | awk -v unit="$unit" '{print $1/unit}'`
-xmax=`gmt gmtinfo $originalxyz -C | awk -v unit="$unit" '{print $2/unit}'`
-zmin=`gmt gmtinfo $originalxyz -C | awk -v unit="$unit" '{print $3/unit}'`
-zmax=`gmt gmtinfo $originalxyz -C | awk -v unit="$unit" '{print $4/unit}'`
+xmin=`gmt info $originalxyz -C | awk -v unit="$unit" '{print $1/unit}'`
+xmax=`gmt info $originalxyz -C | awk -v unit="$unit" '{print $2/unit}'`
+zmin=`gmt info $originalxyz -C | awk -v unit="$unit" '{print $3/unit}'`
+zmax=`gmt info $originalxyz -C | awk -v unit="$unit" '{print $4/unit}'`
 
 width=2.2
 height=`echo "$width*(($zmax)-($zmin))/(($xmax)-($xmin))" | bc -l`
@@ -70,11 +39,10 @@ colorbar_height=0.16
 colorbar_horizontal_position=`echo "$width+0.1" | bc -l`
 colorbar_vertical_position=`echo "$colorbar_width/2" | bc -l`
 domain=$colorbar_horizontal_position\i/$colorbar_vertical_position\i/$colorbar_width\i/$colorbar_height\i
-
 #--------------------------------------------------------------------
 
-amplitude_min=`gmt gmtinfo $originalxyz -C | awk '{print $5}'`
-amplitude_max=`gmt gmtinfo $originalxyz -C | awk '{print $6}'`
+amplitude_min=`gmt info $originalxyz -C | awk '{print $5}'`
+amplitude_max=`gmt info $originalxyz -C | awk '{print $6}'`
 
 scalarLowerLimit=0
 scalarUpperLimit=1
@@ -86,35 +54,33 @@ awk -v unit="$unit" -v scale="$scale" '{print $1/unit, $2/unit, $3/scale}' $orig
 column_number=`head -n 1 $originalxyz | awk '{print NF}'`
 if [ $column_number -eq 3 ]
 then
-original_cpt=GMT_seis.cpt
+cpt=GMT_seis.cpt
 elif [ $column_number -eq 6 ]
 then
-original_cpt=GMT_hot.cpt
+cpt=GMT_hot.cpt
 awk -v unit="$unit" -v scale="$amplitude_max" '{print $1/unit, $2/unit, $5/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$vectorLowerLimit -Lu$vectorUpperLimit -R$region -I$inc -G$xgrd
 awk -v unit="$unit" -v scale="$amplitude_max" '{print $1/unit, $2/unit, $6/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$vectorLowerLimit -Lu$vectorUpperLimit -R$region -I$inc -G$zgrd
 fi
 
 #-----------------------------------------------------
-gmt makecpt -C$original_cpt -T$scalarLowerLimit/$scalarUpperLimit -Iz > $cpt
+gmt begin $fig
+gmt makecpt -C$cpt -T$scalarLowerLimit/$scalarUpperLimit -Iz
 
-gmt psbasemap -R$region -J$projection -Bx10f5+l"X ($unit\m) " -By10f5+l"Z ($unit\m)" -K > $ps
-gmt grdimage -R -J -B $grd -C$cpt -O -K >> $ps
+gmt basemap -R$region -J$projection -BWeSn -Bx10f5+l"X ($unit\m) " -By10f5+l"Z ($unit\m)"s
+gmt grdimage $grd
 
 if [ $column_number -eq 6 ]
 then
-gmt grdvector $xgrd $zgrd -J -Ix1 -Q0.1i+eAl+n0.25i+h0.1 -W1p,gray -S20i -N -O -K >> $ps
+gmt grdvector $xgrd $zgrd -Ix1 -Q0.1i+eAl+n0.25i+h0.1 -W1p,gray -S20i -N
 fi
 
-awk -v unit="$unit" '{print $1/unit, $2/unit}' $backupFolder/positive_finger | gmt psxy -R -J -Ss0.005i -Gred   -N -O -K >> $ps
-awk -v unit="$unit" '{print $1/unit, $2/unit}' $backupFolder/negative_finger | gmt psxy -R -J -Ss0.005i -Ggreen -N -O -K >> $ps
+awk -v unit="$unit" '{print $1/unit, $2/unit}' $backupFolder/positive_finger | gmt plot -Ss0.005i -Gred   -N
+awk -v unit="$unit" '{print $1/unit, $2/unit}' $backupFolder/negative_finger | gmt plot -Ss0.005i -Ggreen -N
 
-gmt psscale -Dx$domain -C$cpt -Bxa1f0.5 -By+l"$scale$label"  -O >> $ps
-
-gmt psconvert -A -Tf $ps -D$figFolder
-rm -f $cpt $grd $xgrd $zgrd
-rm -f $ps
+gmt colorbar -Dx$domain -Bxa1f0.5 -By+l"$scale$label"
+gmt end
 #-----------------------------------------------------
-rm -f gmt.conf
-rm -f gmt.history
+rm -f $grd $xgrd $zgrd
+
 #module unload gmt
 conda deactivate
