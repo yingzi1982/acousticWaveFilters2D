f9df041ac986df586ec8e61bf6be1ce60abeb075
Yingzi1982 <yingzi.ying@me.com>
2022-06-16 10:21:27 +0200

    pushing to Github

--- gmt/test.sh
+++ gmt/test.sh
@@ -2,7 +2,7 @@
 source /pdc/software/21.11/eb/software/Anaconda3/2021.05/bin/activate
 conda activate gmt6
 gmt begin ../figures/symbols
-gmt plot -R0/10/0/10 -JX10c/10c -Baf -Sc0.5c -W1p,black -Gred << EOF
+gmt plot -R0/10/0/10 -JX2.2i/1.4i -Baf -Sc0.5c -W1p,black -Gred << EOF
 2 3
 5 6
 8 2
