2add0bd12696102126f75891103fbd323c5d88a6
Yingzi1982 <yingzi.ying@me.com>
2022-06-17 14:41:44 +0200

    pushing to Github

--- gmt/plot2D.sh
+++ gmt/plot2D.sh
@@ -107,7 +107,7 @@ then
 gmt begin $fig
 gmt makecpt -C$cpt -T$vectorLowerLimit/$vectorUpperLimit -Iz
 #gmt subplot begin 2x1 -M0.2i -Fs$width\i/0 -R$region -J$projection -A+jTR+o8p 
-gmt subplot begin 2x1 -M0.01i/0.01i -Fs5i/0 -R$region -J$projection -A+jTR+o8p
+gmt subplot begin 2x1 -M0.0i/0.0i -Fs$width\i/0 -R$region -J$projection -A+jTR+o8p
 
 gmt subplot set 0,0 
 gmt grdimage $xgrd -Bwesn -Bx10f5+l"$xlabel ($xscale$xunit)" -By10f5+l"$zlabel ($zscale$zunit)"
