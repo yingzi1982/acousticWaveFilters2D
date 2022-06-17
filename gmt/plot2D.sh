5942fce9aa002571008f1891333911aecbbc75bd
Yingzi1982 <yingzi.ying@me.com>
2022-06-17 15:31:17 +0200

    pushing to Github

--- gmt/plot2D.sh
+++ gmt/plot2D.sh
@@ -67,19 +67,19 @@ vectorUpperLimit=1
 
 if [ $type == 'S' ] || [ $type == 'V1' ]
 then
-awk -v xscale="$xscale" -v zscale="$zscale" -v scale="$scale" '{print $1/xscale, $2/zscale, $3/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$scalarLowerLimit -Lu$scalarUpperLimit -R$region -I$inc -G$grd
+awk -v xscale="$xscale" -v zscale="$zscale" -v scale="$amplitude_max" '{print $1/xscale, $2/zscale, $3/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$scalarLowerLimit -Lu$scalarUpperLimit -R$region -I$inc -G$grd
 fi
 
 if [ $type == 'V1' ]
 then
-awk -v xscale="$xscale" -v zscale="$zscale" -v scale="$scale" '{print $1/xscale, $2/xscale, $5/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$vectorLowerLimit -Lu$vectorUpperLimit -R$region -I$inc -G$xgrd
-awk -v xscale="$xscale" -v zscale="$zscale" -v scale="$scale" '{print $1/xscale, $2/xscale, $6/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$vectorLowerLimit -Lu$vectorUpperLimit -R$region -I$inc -G$zgrd
+awk -v xscale="$xscale" -v zscale="$zscale" -v scale="$amplitude_max" '{print $1/xscale, $2/xscale, $5/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$vectorLowerLimit -Lu$vectorUpperLimit -R$region -I$inc -G$xgrd
+awk -v xscale="$xscale" -v zscale="$zscale" -v scale="$amplitude_max" '{print $1/xscale, $2/xscale, $6/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$vectorLowerLimit -Lu$vectorUpperLimit -R$region -I$inc -G$zgrd
 fi
 
 if [ $type == 'V2' ]
 then
-awk -v xscale="$xscale" -v zscale="$zscale" -v scale="$scale" '{print $1/xscale, $2/xscale, $5/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$vectorLowerLimit -Lu$vectorUpperLimit -R$region -I$inc -G$xgrd
-awk -v xscale="$xscale" -v zscale="$zscale" -v scale="$scale" '{print $1/xscale, $2/xscale, $6/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$vectorLowerLimit -Lu$vectorUpperLimit -R$region -I$inc -G$zgrd
+awk -v xscale="$xscale" -v zscale="$zscale" -v scale="$amplitude_max" '{print $1/xscale, $2/xscale, $5/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$vectorLowerLimit -Lu$vectorUpperLimit -R$region -I$inc -G$xgrd
+awk -v xscale="$xscale" -v zscale="$zscale" -v scale="$amplitude_max" '{print $1/xscale, $2/xscale, $6/scale}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$vectorLowerLimit -Lu$vectorUpperLimit -R$region -I$inc -G$zgrd
 fi
 #-----------------------------------------------------
 if [ $type == 'S' ] || [ $type == 'V1' ]
