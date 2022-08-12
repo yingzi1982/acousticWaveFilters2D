82d2646a5d667b5ca71c196998482ca622ad8ddf
Yingzi1982 <yingzi.ying@me.com>
2022-08-12 11:27:15 +0200

    pushing to Github

--- gmt/plotSpectrogram.sh
+++ gmt/plotSpectrogram.sh
@@ -61,7 +61,15 @@ gmt makecpt -CGMT_seis.cpt -Iz -T$specgramLowerLimit/$specgramUpperLimit
 
 awk -v tscale="$tscale" -v fscale="$fscale" '{print $1/tscale, $2/fscale, $3}' $originalxyz | gmt blockmean -R$region -I$inc | gmt surface -Ll$specgramLowerLimit -Lu$specgramUpperLimit -R$region -I$inc -G$grd
 
-gmt grdimage $grd -R$region -J$projection -BWeSn -Bx10f5+l"Time ($tscale\s)" -By5f2.5+l"Frequency ($fscale\Hz)"
+gmt grdimage $grd -R$region -J$projection -BWeSn -Bx10f5+l"Time ($tscale\s)" -By10f5+l"Freq ($fscale\Hz)"
+
+
+colorbar_width=$height
+colorbar_height=0.16
+colorbar_horizontal_position=`echo "$width+0.1" | bc -l`
+colorbar_vertical_position=`echo "$colorbar_width/2" | bc -l`
+domain=$colorbar_horizontal_position\i/$colorbar_vertical_position\i/$colorbar_width\i/$colorbar_height\i
+gmt colorbar -Dx$domain -Bxa50f25 -By+l"dB"
 
 gmt end
 #-----------------------------------------------------
