141f920ff9bf27aada9dfb2524c5d0776100d840
Yingzi1982 <yingzi.ying@me.com>
2022-08-17 13:28:51 +0200

    pushing to Github

--- bash/gmt.sh
+++ bash/gmt.sh
@@ -92,5 +92,5 @@ if false; then
 ./plotSpectrogram.sh
 fi
 if true; then
-./plot1DSignal.sh txy 1 Freq 1E9 Hz "0 10" 5f2.5 Txy 1E0 "dB" "-15 0" 5f.25
+./plot1DSignal.sh txy 1 Freq 1E9 Hz "0 10" 5f2.5 Txy 1E0 "dB" "-15 0" 5f2.5
 fi
