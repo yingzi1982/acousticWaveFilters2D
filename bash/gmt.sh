c63e319a25a0f2860688e8a6f859320d81e22634
Yingzi1982 <yingzi.ying@me.com>
2022-08-18 10:30:19 +0200

    pushing to Github

--- bash/gmt.sh
+++ bash/gmt.sh
@@ -92,7 +92,8 @@ if false; then
 ./plotSpectrogram.sh
 fi
 if true; then
-./plot1DSignal.sh sourceTimeFunction 10 Time 1E-9 s "0 5" 5f2.5 Amp. 1E0 "v" "-1 1" 1f0.5
+./plot1DSignal.sh sourceTimeFunction 10 Time 1E-9 s "0 5" 5f2.5 Amp 1E0 "V" "-1 1" 1f0.5
+./plot1DSignal.sh sourceFrequencySpetrum 10 Freq 1E9 s "0 10" 5f2.5 Amp 1E0 "V/Hz" "-1 1" 1f0.5
 ./plot1DSignal.sh current 10 Time 1E-9 s "0 40" 20f10 Amp. 1E0 "" "-.04 .04" 1f0.5
 fi
 if false; then
