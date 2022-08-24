edfbf792f90f6dd9eb7905ea7e6d24d36b195361
Yingzi1982 <yingzi.ying@me.com>
2022-08-24 17:06:32 +0200

    pushing to Github

--- bash/gmt.sh
+++ bash/gmt.sh
@@ -98,5 +98,5 @@ if true; then
 ./plot1DSignal.sh  charge 10 Time 1E-9 s "0 20" 10f5 Charge 1E-11 "C" "-1 1" 1f0.5
 ./plot1DSignal.sh current 10 Time 1E-9 s "0 20" 10f5 Current  1E-1 "A" "-1 1" 0.5f0.25
 #./plot1DSignal.sh admittance_spectrum 1 Freq 1E9 Hz "0 3" 1f0.5 Amp 1E0 "" "-50 0" 25f12.5
-./plot1DSignal.sh admittance_spectrum 1 Freq 1E9 Hz "0.1 2.9" 1f0.5 Amp 1E0 "" "0 0.4" 0.1f.05
+./plot1DSignal.sh admittance_spectrum 1 Freq 1E9 Hz "0.1 2.9" 1f0.5 Amp 1E0 "" "0 250" 100f50
 fi
