0d28ff95609a48d7a39d3097f04d5234a06e8113
Yingzi1982 <yingzi.ying@me.com>
2022-08-29 13:35:14 +0200

    pushing to Github

--- slurm/specfem.sh
+++ slurm/specfem.sh
@@ -3,10 +3,10 @@
 #SBATCH -A snic2022-22-620 #pdc-test-2022
 #SBATCH -p main #main long shared memory
 #SBATCH -J specfem
-#SBATCH -t 5:00:00
+#SBATCH -t 10:00:00
 #SBATCH -o output.txt
 #SBATCH -e error.txt
-#SBATCH -N 1
+#SBATCH -N 2
 #SBATCH --mail-user=yingzi.ying@me.com
 #SBATCH --mail-type=ALL
 
@@ -16,6 +16,6 @@ filter_type=SAW
 filter_dimension=2D
 
 cd ../bash
-./preprocess.sh $filter_type $filter_dimension
+#./preprocess.sh $filter_type $filter_dimension
 ./specfem.sh $filter_dimension
 #./postprocess.sh $filter_type $filter_dimension
