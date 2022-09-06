563ed4419c053badd458f6f2fba55ccfcccfa92d
Yingzi1982 <yingzi.ying@me.com>
2022-09-06 14:44:57 +0200

    pushing to Github

--- bash/gmt.sh
+++ bash/gmt.sh
@@ -105,7 +105,16 @@ if true; then
 #./plot1DSignal.sh current 10 Time 1E-9 s "0 20" 10f5 Current  1E "A" "-1 1" 0.5f0.25
 #./plot1DSignal.sh sourceTimeFunction 10 Time 1E-8 s "0 10" 4f2 A  1 "A" "-1 1" 1f0.5
 #./plot1DSignal.sh PF_charge_piezo 10 Time 1E-8 s "0 10" 4f2 A  2E-11 "A" "-1 1" 1f0.5
-./plot1DSignal.sh admittance 1 Freq 1E9 Hz "0 3" 1f0.5 Amp 1E0 "" "-0.2 0.3" ''
+
+admittance_file=../backup/admittance
+admittance_real_file=../backup/admittance_real
+admittance_imag_file=../backup/admittance_imag
+cat $admittance_file | awk  '{print $1,$2}' > $admittance_real_file
+cat $admittance_file | awk  '{print $1,$3}' > $admittance_imag_file
+./plot1DSignal.sh admittance_real 1 Freq 1E9 Hz "0 3" 1f0.5 Amp 1E0 "" "-0.3 0.3" ''
+./plot1DSignal.sh admittance_imag 1 Freq 1E9 Hz "0 3" 1f0.5 Amp 1E0 "" "-0.3 0.3" ''
+rm -f $admittance_real_file $admittance_imag_file
+exit
 #./plot1DSignal.sh admittance_spectrum 1 Freq 1E9 Hz "0 3" 1f0.5 Amp 1E0 "" "-50 0" 25f12.5
 #./plot1DSignal.sh admittance_spectrum 1 Freq 1E9 Hz "0.1 2.9" 1f0.5 Amp 1E0 "" "-30 0" 10f5
 
