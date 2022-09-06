9688a3e82295c26fc6be42e21c7b6bc0b60f3ca6
Yingzi1982 <yingzi.ying@me.com>
2022-09-06 13:02:02 +0200

    pushing to Github

--- bash/gmt.sh
+++ bash/gmt.sh
@@ -105,7 +105,7 @@ if true; then
 #./plot1DSignal.sh current 10 Time 1E-9 s "0 20" 10f5 Current  1E "A" "-1 1" 0.5f0.25
 #./plot1DSignal.sh sourceTimeFunction 10 Time 1E-8 s "0 10" 4f2 A  1 "A" "-1 1" 1f0.5
 ./plot1DSignal.sh PF_charge_piezo 10 Time 1E-8 s "0 10" 4f2 A  2E-11 "A" "-1 1" 1f0.5
-./plot1DSignal.sh admittance 1 Freq 1E9 Hz "0 3" 1f0.5 Amp 1E0 "" ".1 0.5" .1f.05
+./plot1DSignal.sh admittance 1 Freq 1E9 Hz "0 3" 1f0.5 Amp 1E0 "" ".0 0.5" .1f.05
 #./plot1DSignal.sh admittance_spectrum 1 Freq 1E9 Hz "0 3" 1f0.5 Amp 1E0 "" "-50 0" 25f12.5
 #./plot1DSignal.sh admittance_spectrum 1 Freq 1E9 Hz "0.1 2.9" 1f0.5 Amp 1E0 "" "-30 0" 10f5
 
