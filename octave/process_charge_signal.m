b87d35df5d55a4340d03506cdf1a09a8be5b2799
Yingzi1982 <yingzi.ying@me.com>
2022-09-22 13:37:56 +0200

    pushing to Github

---
+++ octave/process_charge_signal.m
@@ -0,0 +1,28 @@
+#!/usr/bin/env octave
+
+clear all
+close all
+clc
+
+charge = dlmread('../backup/charge','');
+voltage = dlmread(['../backup/sourceTimeFunction'],'');
+
+if rows(charge) != rows(voltage)
+  error('the charge and voltage are not equal length!')
+end
+t = charge(:,1);
+timeIndex =  find(t<=12e-8); 
+charge = charge(timeIndex,:);  
+voltage = voltage(timeIndex,:); 
+
+voltage_spectrum = trace2spectrum(voltage);
+charge_spectrum = trace2spectrum(charge);
+f = voltage_spectrum(:,1);
+admittance = -i*2*pi*f.*charge_spectrum(:,2)./voltage_spectrum(:,2);
+freqIndex = find(f>0.5e9&f<1.5e9);
+admittance = [abs(admittance) real(admittance) imag(admittance)];
+min(admittance(freqIndex))
+max(admittance(freqIndex))
+
+admittance = [f, admittance];
+dlmwrite('../backup/admittance',admittance,' ');
