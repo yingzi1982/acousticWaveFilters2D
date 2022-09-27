a9daf3c88f8c1285b94791f5568912e4e66ab69e
Yingzi1982 <yingzi.ying@me.com>
2022-09-27 11:23:04 +0200

    pushing to Github

--- bash/gmt.sh
+++ bash/gmt.sh
@@ -37,7 +37,7 @@ if false; then
 ./plot2DField.sh bodyforce V2  '-CGMT_seis.cpt -Iz'  1E13 $bodyforce_unit $heightRatio $dx X 1E-6 m $xtick $dz Z 1E-6 m $ztick
 fi
 #--------------------------------------------------
-if false; then
+if true; then
 for i in $(seq 1 35)
 do
 snapshot=snapshot_$i
